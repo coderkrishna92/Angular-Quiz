7429669b71f1d428520cd5b0a6621b7416986000
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-10-27 12:25:33 -0400

    Ensuring to have the necessary WebAPI url properly defined

--- src/app/shared/quiz.service.ts
+++ src/app/shared/quiz.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class QuizService {
 
   /*--Properties--*/
-  readonly rootUrl = '';
+  readonly rootUrl = 'http://localhost:53152';
 
 
   /*--Helper methods--*/

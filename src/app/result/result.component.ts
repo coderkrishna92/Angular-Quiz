e12f1ab7941b3ed5567f42e9586e34c0abb8b6c2
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-11-01 20:24:56 -0400

    Making necessary changes right now

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -13,27 +13,17 @@ export class ResultComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    if (parseInt(localStorage.getItem('quProgress')) == 10) {
-      this.quizService.seconds = parseInt(localStorage.getItem('seconds'));
-      this.quizService.qnProgress = parseInt(localStorage.getItem('qnProgress'));
-      this.quizService.qns = JSON.parse(localStorage.getItem('qns'));
-
     this.quizService.getAnswers().subscribe(
       (data: any) => {
-        // Resetting the correct answer count to 0
         this.quizService.correctAnswerCount = 0;
-
-        // The 10 random questions array
         this.quizService.qns.forEach((e, i) => {
-          if(e.answer == data[i].answer) {
+          if(e.answer == data[i]) {
             this.quizService.correctAnswerCount++;
             e.correct = data[i];
           }
         });
-      });
-    } else {
-      this.router.navigate(['/quiz']);
-    }
+      }
+    )
   }
 
   OnSubmit() {
